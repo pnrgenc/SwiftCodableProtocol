6e2b06777252325c91a0da5c5e8038fee15f214e
pinargenc <gncpin@hotmail.com>
2020-05-03 12:48:40 +0300

    Initial Commit

---
+++ SwiftCodableProtocol/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftCodableProtocol
+//
+//  Created by Pınar Genç on 3.05.2020.
+//  Copyright © 2020 Pınar Genç. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
